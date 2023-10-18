58a5a0f6baf13de5e768e3a465572bf2a2c26aa7
tolgagalipaslan <tolgagalipaslan1999@gmail.com>
2023-10-18 20:14:12 +0300

    some changes

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const index = () => {
-  return (
-    <div>
-      sa
-    </div>
-  );
-}
+  return <div>sa</div>;
+};
 
 export default index;
